4766686a664f02c38edc9a61642a6f9c54de5d9a
filips321 <filipsmejda@icloud.com>
2024-08-06 15:38:48 +0200

    AWS Lambda compatibility

--- src/main.py
+++ src/main.py
@@ -3,9 +3,9 @@ from bottelegram import *
 import asyncio
 from datetime import date
 
-# import emoji
 
-if __name__ == "__main__":
+# Option 1 - AWS
+def lambda_handler(event, context):
     df_weather = download_weather(read_geo_data())
     plots = plot_charts(df_weather)
 
@@ -18,3 +18,23 @@ if __name__ == "__main__":
     asyncio.run(send_message_telegram(read_credentials(), plots[1]))
     asyncio.run(send_message_telegram(read_credentials(), plots[2]))
     asyncio.run(send_message_telegram(read_credentials(), '========== \U0001F9FF =========='))
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Lambda implemented properly!')
+    }
+
+
+# Option 2 - local
+# if __name__ == "__main__":
+#     df_weather = download_weather(read_geo_data())
+#     plots = plot_charts(df_weather)
+#
+#     asyncio.run(send_message_telegram(read_credentials(),
+#                                       'Wake up BRO, it\'s 6am already! \U0001f976\n\nYour daily weather report for:\n' + str(
+#                                           round(read_geo_data()['latitude'], 2)) + ', ' + str(
+#                                           round(read_geo_data()['longitude'], 2)) + '\n' + str(
+#                                           date.today().strftime('%d/%m/%Y'))))
+#     asyncio.run(send_message_telegram(read_credentials(), plots[0]))
+#     asyncio.run(send_message_telegram(read_credentials(), plots[1]))
+#     asyncio.run(send_message_telegram(read_credentials(), plots[2]))
+#     asyncio.run(send_message_telegram(read_credentials(), '========== \U0001F9FF =========='))
